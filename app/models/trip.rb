dfc6c4494f7e20b4f801ec2e3eafcf68586ca100
Salami, Tope <salami.tope@gmail.com>
2016-03-29 23:00:18 +0900

    added destinations scaffold

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ActiveRecord::Base
+	
+
+	has_many :destinations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328125725) do
+ActiveRecord::Schema.define(version: 20160329133314) do
+
+  create_table "destinations", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "trip_id"
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string   "name"
